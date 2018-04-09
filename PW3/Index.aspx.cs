5c391a4a91cbb4383f9d66bfc4f3340282307e4d
Jasper Peijer <j.peijer@appec.nl>
2018-04-09 12:25:37 +0200

    Created project

---
+++ PW3/Index.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PW3
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+         
+        }
+
+        public void RenderSjoerd()
+        {
+            Response.WriteFile("Include/IndexSjoerd.aspx");
+        }
+
+        public void RenderJasper()
+        {
+            Response.WriteFile("Include/IndexJasper.aspx");
+        }
+
+        public void RenderFooter()
+        {
+            Response.WriteFile("Include/IndexFooter.aspx");
+        }
+    }
+}
